5fa0270195da8aeaf5c1e44874fb317c1c49ff17
Ismail Ceylan <ismaillceylan@gmail.com>
2023-12-20 20:53:27 +0300

    refactor: old stuff cleaning

--- app/Providers/RouteParamsResolvers.php
+++ app/Providers/RouteParamsResolvers.php
@@ -3,36 +3,18 @@
 namespace App\Providers;
 
 use Garavel\Routing\ParamResolver;
-use App\Http\Controllers\Api\BaseController;
 
 class RouteParamsResolvers extends ParamResolver
 {
+	/**
+	 * Registers resolvers to classes. With that Garavel
+	 * can know how to resolve when it see that classes.
+	 *
+	 * @return void
+	 */
 	public function register()
 	{
-		static::resolve(
-			\JDB\Database::class,
-			fn( $_, $routeArgs ) => $this->db( $routeArgs->dbname )
-		);
-
-		static::resolve(
-			\JDB\Table::class,
-			fn( $_, $routeArgs ) => $this->db( $routeArgs->dbname )->table( $routeArgs->tablename )
-		);
-
-		static::resolve(
-			\JDB\Row::class,
-			fn( $_, $routeArgs ) =>
-				$this
-					->db( $routeArgs->dbname )
-					->table( $routeArgs->tablename )
-					->find( $routeArgs->id )
-		);
+		// 
 	}
 
-	private function db( $name )
-	{
-		return \JDB\JDB::connect(
-			BaseController::DATAPATH . DIRECTORY_SEPARATOR . $name
-		);
-	}
 }
